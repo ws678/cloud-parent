38c39f42e875b15eedea548e9b343ca5b64d28ac
ws678 <3190552680@qq.com>
2022-05-21 10:36:14 +0800

    2.搭建eureka集群

--- cloud-eureka3/src/main/java/com/sdkj/App.java
+++ cloud-eureka3/src/main/java/com/sdkj/App.java
@@ -1,18 +1,17 @@
 package com.sdkj;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author wangshuo
- * @Date 2022/5/18, 16:37
+ * @Date 2022/5/18, 9:24
  * Please add a comment
  */
+//标识为EurekaServer
+@EnableEurekaServer
 @SpringBootApplication(scanBasePackages = {"com.sdkj"})
-@EnableEurekaClient
-@MapperScan("com.sdkj.dao")//扫描dao添加Mapper
 public class App {
 
     public static void main(String[] args) {
